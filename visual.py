d29d7232f714b307596756d317928804b420432a
Kirjia <40699415+Kirjia@users.noreply.github.com>
2023-03-08 13:44:47 +0100

    oversampling, k-fold validation on an DecisionTree

--- visual.py
+++ visual.py
@@ -5,11 +5,15 @@ import sklearn
 import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 from collections import Counter
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import StratifiedKFold
+
 
 dataUni = str("Star99999_raw.csv")
 
@@ -39,7 +43,8 @@ print(data["SpType"].value_counts())
 def func1(x):
     return x / 1000
 
-def sptClass(x : str):
+
+def sptClass(x: str):
     if x.__contains__('M'):
         return 6
     elif x.__contains__('K'):
@@ -55,6 +60,7 @@ def sptClass(x : str):
     elif x.__contains__('O'):
         return 0
 
+
 def target(x):
     if x.__contains__('V'):
         return 0
@@ -66,7 +72,7 @@ def target(x):
 '''data['Plx'] = data['Plx'].map(lambda x: func1(x))
 data['e_Plx'] = data['e_Plx'].map(lambda x: func1(x))'''
 
-data.drop(data[((data["e_Plx"] * 100) / data["Plx"]) >= 18].index, inplace=True)
+data.drop(data[((data["e_Plx"] * 100) / data["Plx"]) >= 11].index, inplace=True)
 
 Amag = []
 
@@ -84,28 +90,72 @@ data['SpType'] = data['SpType'].map(lambda x: sptClass(x))
 
 data.dropna(how="any", inplace=True)
 
+
 data.info()
 print("min: {} max: {}".format(data["e_Plx"].min(), data["e_Plx"].max()))
 
-data.to_csv('Stars.csv', index=False)
 smote = SMOTE(random_state=42)
 
-X, Y = data.iloc[:, :5].values, data.iloc[:, 6].values
+X, Y = data.iloc[:, :5], data.iloc[:, 6]
+stdc = StandardScaler()
+X_std = stdc.fit_transform(X)
+
 
 
 
 #X_train_smote, X_test_smote, Y_train_smote, Y_test_smote = train_test_split(X_smote, Y_smote, test_size=0.3, random_state=42)
 
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+X_smote, Y_smote = smote.fit_resample(X_std, Y)
+#X_train, X_test, Y_train, Y_test = train_test_split(X_smote, Y_smote, test_size=0.3, random_state=42)
 
-#X_smote, Y_smote = smote.fit_resample(data.iloc[:, 1:].values, data['TargetClass'])
-X_smote, Y_smote = smote.fit_resample(X, Y)
 
+'''
 stdc = StandardScaler()
-X_train_std = stdc.fit_transform(X_train)
+X_train_std = stdc.fit_transform(X_smote)
 X_test = stdc.transform(X_test)
+'''
 
 
 print('Original dataset shape %s' % Counter(Y))
 print('Resampled dataset shape %s' % Counter(Y_smote))
+
+
+'''
+tree_model = DecisionTreeClassifier(max_depth=40)
+tree_model.fit(X_train_std, Y_smote)
+Y_pred = tree_model.predict(X_test)
+labels = np.unique(Y_test)
+
+print(confusion_matrix(Y_test, Y_pred, labels=labels))
+print(classification_report(Y_test, Y_pred))
+
+print("Accuracy:", metrics.accuracy_score(Y_test, Y_pred)
+'''
+
+
+kfold = StratifiedKFold(n_splits=10, random_state=None, shuffle=False)
+tree_model = DecisionTreeClassifier(max_depth=30)
+scores = []
+
+for k, (train_index, test_index) in enumerate(kfold.split(X_smote, Y_smote)):
+    tree_model.fit(X_smote[train_index], Y_smote[train_index])
+    Y_pred = tree_model.predict(X_smote[test_index])
+    labels = np.unique(Y_smote[test_index])
+    conMat = confusion_matrix(Y_smote[test_index], Y_pred, labels=labels)
+    report = classification_report(Y_smote[test_index], Y_pred)
+    score = metrics.accuracy_score(Y_smote[test_index], Y_pred)
+    result = {}
+    result.__setitem__('confusionMatrix', conMat)
+    result.__setitem__('report', report)
+    result.__setitem__('Acc', score)
+    scores.append(result)
+    print('Fold: {} Class dist.: {}, Acc: {}'.format(k+1, np.bincount(Y_smote[train_index]), score))
+
+
+for item in scores:
+    print(item['confusionMatrix'])
+    print(item['report'])
+    print(item['Acc'])
+
+del scores
